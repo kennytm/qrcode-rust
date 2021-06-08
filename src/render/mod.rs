80d56673edb6677b07eade196607a22858190530
CodeAssemblingChicken <53775955+CodeAssemblingChicken@users.noreply.github.com>
2021-06-08 20:02:04 +0200

    Fixed comment layout

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -98,6 +98,7 @@ impl<'a, P: Pixel> Renderer<'a, P> {
     }
 
     /// Sets the size of the quiet zone in the generated image.
+    ///
     /// Default: 4 for Normal QR code and 2 for Micro QR code
     pub fn quiet_zone_size(&mut self, quiet_zone: u32) -> &mut Self {
         self.quiet_zone = quiet_zone;
